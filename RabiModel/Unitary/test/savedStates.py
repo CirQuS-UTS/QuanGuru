8f2d022224a1c2ed2fa600813594e462d0ee28f7
cahitkargi <13433634@student.uts.edu.au>
2019-09-02 21:31:15 +1000

    new script for long time and frequency range

--- RabiModel/Unitary/test/savedStates.py
+++ RabiModel/Unitary/test/savedStates.py
@@ -9,9 +9,9 @@ from QuantumToolbox.operators import parityEXP
 from classes.parameterObj import ParamObj
 import scipy.sparse as sp
 
-data = readData(1567416516.606023, 'states')
+data = readData(1567423427.773038, 'states')
 
-parityOp = parityEXP(sp.kron(number(200), identity(2), format='csc')).toarray()
+parityOp = parityEXP(sp.kron(number(20), identity(2), format='csc')).toarray()
 
 def ex(states):
     par = []
@@ -30,6 +30,13 @@ p.join()
 end = datetime.datetime.now()
 
 rabiParams = ParamObj('rabiParams')
+rabiParams.sweepMax = 3
+rabiParams.sweepMin = -3
+rabiParams.StepSize = 0.02
+rabiParams.sweepPerturbation = 0.01
+rabiParams.resonatorDimension = 20
+rabiParams.sweepKey = 'resonator Frequency'
+rabiParams.finalTime = 6
 Y, X = np.meshgrid(rabiParams.times, rabiParams.sweepList)
 Z = parity
 
