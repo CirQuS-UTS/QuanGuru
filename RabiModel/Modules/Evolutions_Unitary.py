8967e9f3f7bf1634a2519bb1e3dde625a51a0988
cahitkargi <13433634@student.uts.edu.au>
2019-09-03 13:27:37 +1000

    finite-time bitflip in DQRS is included

--- RabiModel/Modules/Evolutions_Unitary.py
+++ RabiModel/Modules/Evolutions_Unitary.py
@@ -21,7 +21,10 @@ def digitalRabi(obj,stepSize):
               )
 
     UnitaryJC = liou.Liouvillian(HamJC, timeStep=(stepSize / 2))
-    UnitaryAJC = UnitaryJC @ UnitaryJC
+    if obj.qubitFreqAJC == obj.qubitFreqJC:
+        UnitaryAJC = UnitaryJC @ UnitaryJC
+    else:
+        UnitaryAJC = liou.Liouvillian(HamAJC, timeStep=stepSize)
     Unitary = UnitaryJC @ obj.sigmaX @ UnitaryAJC @ obj.sigmaX @ UnitaryJC
     return Unitary
 
@@ -37,4 +40,41 @@ def idealRabi(obj,stepSize):
 
 
     Unitary = liou.Liouvillian(HamRabi, timeStep=stepSize)
+    return Unitary
+
+def digitalRabiF(obj,stepSize):
+    HamJC = (2 * np.pi *
+             (
+                ((obj.offset + (obj.resonatorFrequency / obj.ratio)) * obj.HamiltonianCavity) +
+                ((obj.offset + obj.qubitFreqJC) * obj.HamiltonianQubit) +
+                ((obj.offset + obj.g) * obj.couplingJC)
+             )
+             )
+    HamAJC = (2 * np.pi *
+              (
+                ((obj.offset + (obj.resonatorFrequency / obj.ratio)) * obj.HamiltonianCavity) +
+                ((obj.offset + obj.qubitFreqAJC) * obj.HamiltonianQubit) +
+                ((obj.offset + obj.g) * obj.couplingJC)
+              )
+              )
+
+    UnitaryJC = liou.Liouvillian(HamJC, timeStep=(stepSize / 2))
+    if obj.qubitFreqAJC == obj.qubitFreqJC:
+        UnitaryAJC = UnitaryJC @ UnitaryJC
+    else:
+        UnitaryAJC = liou.Liouvillian(HamAJC, timeStep=stepSize)
+
+    if obj.offset == 0:
+        Unitary = UnitaryJC @ obj.sigmaX @ UnitaryAJC @ obj.sigmaX @ UnitaryJC
+    else:
+        HamJC = (2 * np.pi *
+                 (
+                         ((obj.offset + (obj.resonatorFrequency / obj.ratio)) * obj.HamiltonianCavity) +
+                         ((obj.offset + obj.g) * obj.couplingJC)
+                 )
+                 )
+        half_flip = liou.Liouvillian(HamJC)
+        UnitaryFlip = liou.Liouvillian(half_flip,timeStep=(obj.bitflipTime/2))
+        Unitary = UnitaryJC @ UnitaryFlip @ obj.sigmaX @ UnitaryFlip @ UnitaryAJC @ UnitaryFlip @ obj.sigmaX @ UnitaryFlip @ UnitaryJC
+
     return Unitary
