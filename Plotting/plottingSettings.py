caa61b6b0a903305c5d2e4e961c6a6a8d38cc411
cahitkargi <13433634@student.uts.edu.au>
2019-10-31 17:05:58 +1100

    some default plotting parameters changed

--- Plotting/plottingSettings.py
+++ Plotting/plottingSettings.py
@@ -3,16 +3,16 @@ def plottingSet(ax):
     ax.tick_params('both', which='minor', width=1, length=2)
     ax.tick_params('both', which='major', width=1, length=4)
     #######
-    """for axis in ['bottom', 'left']:
-        ax.spines[axis].set_linewidth(2.0)"""
+    for axis in ['bottom', 'left', 'top', 'right']:
+        ax.spines[axis].set_linewidth(2.0)
     #######
     for tick in ax.xaxis.get_major_ticks():
-        tick.label.set_fontsize(12)
-        # tick.label.set_fontweight('bold')
+        tick.label.set_fontsize(20)
+        tick.label.set_fontweight('bold')
     #######
     for tick in ax.yaxis.get_major_ticks():
-        tick.label.set_fontsize(12)
-        # tick.label.set_fontweight('bold')
+        tick.label.set_fontsize(20)
+        tick.label.set_fontweight('bold')
     #######
     ax.ticklabel_format(axis='y', style='plain')
     ax.ticklabel_format(axis='x', style='plain')
