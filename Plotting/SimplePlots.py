8a0a1e071e4316d99520ee1b852150af619d980f
fabiolous <fabio.a.ferreirahenriques@student.uts.edu.au>
2020-01-23 12:44:50 +1100

    Minor astetic changes

--- Plotting/SimplePlots.py
+++ Plotting/SimplePlots.py
@@ -2,14 +2,15 @@ import matplotlib.pyplot as plt
 import Plotting.Functions as pltFncs
 import numpy as np
 
-def colorPlot(x,y, z, irregular=False):
-    if irregular == False:
+
+def colorPlot(x, y, z, irregular=False):
+    if irregular is False:
         cm = plt.get_cmap('inferno')
         fig2, ax2 = plt.subplots()
         Y, X = np.meshgrid(y, x)
         surf2 = ax2.pcolormesh(X, Y, z, cmap=cm, norm=pltFncs.normalizeCMAP(cm, -1, 1))
         cbar = plt.colorbar(surf2)
-        cbar.set_ticks([-1,0,1])
+        cbar.set_ticks([-1, 0, 1])
         plt.show()
         print('s')
         return surf2
@@ -18,6 +19,7 @@ def colorPlot(x,y, z, irregular=False):
         cbar = plt.colorbar(surf2)
         cbar.set_ticks([-1, 0, 1])
         return surf2
+
 
 def colorPlotIreg(x, y, z):
     fig2, ax2 = plt.subplots()
