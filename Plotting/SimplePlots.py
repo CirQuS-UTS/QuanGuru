278c676dc717fa728409b3ae8db0e6147fd62904
cahitkargi <13433634@student.uts.edu.au>
2020-01-24 11:10:14 +1100

    Modified parameters in simplePlots eigenvec plots

--- Plotting/SimplePlots.py
+++ Plotting/SimplePlots.py
@@ -52,49 +52,63 @@ def colorPlotIreg(x, y, z, maxC=1, minC=-1, mapC = 'PuYlGn'):
     return surf2
 
 
-def _pltEig(xax, Dims, Bins, x, Step=False, logScale=False):
-    plt.hist(xax, bins=Bins, density=True)
+def __pltEig(xax, Dims, Bins=None, x=None, Step=False, logScale=False, ax=None, Legend=True, xlim=0.04):
+    if ax == None:
+        ax = plt.gca()
 
+    if (isinstance(Bins, np.ndarray) == True) or (isinstance(Bins, int)):
+        ax.hist(xax, bins=Bins, density=True)
+    else:
+        ax.hist(xax, density=True)
+    
     COE = [RMTdist.EigenVectorDist(X, int(Dims), 1) for X in x]
     CUE = [RMTdist.EigenVectorDist(X, int(Dims), 2) for X in x]
     CSE = [RMTdist.EigenVectorDist(X, int(Dims), 4) for X in x]
 
     if Step == False:
-        plt.plot(x, COE, 'r-', label=r'COE ($\beta = 1$)')
-        plt.plot(x, CUE, 'g-', label=r'CUE ($\beta = 2$)')
-        plt.plot(x, CSE, 'b-', label=r'CSE ($\beta = 4$)')
+        ax.plot(x, COE, 'r-', label=r'COE ($\beta = 1$)')
+        ax.plot(x, CUE, 'g-', label=r'CUE ($\beta = 2$)')
+        ax.plot(x, CSE, 'b-', label=r'CSE ($\beta = 4$)')
     elif Step == True:
-        plt.step(x, COE, 'r-', label=r'COE ($\beta = 1$)')
-        plt.step(x, CUE, 'g-', label=r'CUE ($\beta = 2$)')
-        plt.step(x, CSE, 'b-', label=r'CSE ($\beta = 4$)')
+        ax.step(x, COE, 'r-', label=r'COE ($\beta = 1$)')
+        ax.step(x, CUE, 'g-', label=r'CUE ($\beta = 2$)')
+        ax.step(x, CSE, 'b-', label=r'CSE ($\beta = 4$)')
 
-    ax = plt.gca()
     pltSet.plottingSet(ax)
-    #ax.set_xticks([0.0, 0.075, 0.15])
-    #ax.set_yticks([0, 100, 200])
+    
     if logScale == True:
         ax.set_xscale('log')
         ax.set_yscale('log')
         plt.xlim([0.00001, 1])
         plt.ylim([0.1, 10*Dims])
     elif logScale == False:
-        ax.set_ylim([0, 0.5 * Dims])
-        ax.set_xlim([0, 0.04])
+        ax.set_ylim([0, Dims])
+        ax.set_xlim([0, xlim])
+        ax.set_yticks([0.0, Dims/2, Dims])
+        ax.set_xticks([0.0, xlim/2, xlim])
 
-    plt.legend(loc='upper right', fontsize='x-large')
+    if Legend == True:
+        plt.legend(loc='upper right', fontsize='x-large')
 
-
-def pltEig(xax, Dims, Bins=None, Step=False, logScale=True, lvl=True):
-    fig = plt.figure(figsize=(12, 9))
+def _pltEig(xax, Dims, ax, Bins=None, Step=False, logScale=True, lvl=True, Legend=True, xLim=0.04):
     if lvl == True:
         if isinstance(Bins, np.ndarray) == True:
             bin_means = (np.histogram(Bins, Bins, weights=Bins)[0] / np.histogram(Bins, Bins)[0])
-            _pltEig(xax, Dims, Bins, bin_means, Step, logScale)
+            __pltEig(xax, Dims, Bins, bin_means, Step, logScale, ax, Legend, xLim)
         else:
             x = np.arange(0.00001, 1, 0.00001)
-            _pltEig(xax, Dims, 50, x, Step, logScale)
+            __pltEig(xax, Dims, Bins, x, Step, logScale, ax, Legend, xLim)
     else:
         plt.hist(xax, density=True)
-    fig.text(0.5, 0.01, r'$|c_n|^2$', ha='center', fontsize=24)
-    fig.text(0.005, 0.5, r'Prob($|c_n|^2$)', va='center', rotation='vertical', fontsize=24)
-    plt.show()
+        return 'nothing'
+
+def pltEig(xax, Dims, Bins=None, Step=False, logScale=True, lvl=True, ax=None, Legend=True, xlim=0.04):
+    if ax == None:
+        fig = plt.figure(figsize=(12, 9))
+        _pltEig(xax, Dims, ax, Bins, Step, logScale, lvl, Legend, xlim)
+        fig.text(0.5, 0.01, r'$|c_n|^2$', ha='center', fontsize=24)
+        fig.text(0.005, 0.5, r'# of $|c_n|^2$', va='center', rotation='vertical', fontsize=24)
+    else:
+        _pltEig(xax, Dims, ax, Bins, Step, logScale, lvl, Legend, xlim)
+
+    
