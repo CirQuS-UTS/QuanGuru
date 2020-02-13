bf526e0b9ce9eae009846ca55ef51ff73234b7f6
cahitkargi <13433634@student.uts.edu.au>
2020-02-13 20:41:44 +1100

    Simulation object with new temp functions

--- qTools/classes/QSim.py
+++ qTools/classes/QSim.py
@@ -4,6 +4,7 @@ import scipy.sparse as sp
 import qTools.QuantumToolbox.states as qSt
 from qTools.classes.QSys import QuantumSystem
 from qTools.classes.QUni import qUniversal
+from functools import partial
 """ under construction """
 
 
@@ -141,11 +142,11 @@ class Simulation(qUniversal):
         self.timeSweep.sweepMin = 0
         self.timeSweep.sweepPert = 0.02
         self.allStates = True
+        self.__tProtocol = None
         self._qUniversal__setKwargs(**kwargs)
 
     def __del__(self):
         class_name = self.__class__.__name__
-
 
     @property
     def qSys(self):
@@ -159,87 +160,23 @@ class Simulation(qUniversal):
     @property
     def times(self):
         return self.timeSweep.sweepList
-
  
     def addSweep(self, obj, label):
         newSwe = self.sweep.addSweep(sys=obj, label=label)
         return newSwe
 
-    def evolveTimeIndep(self, obj, sweep):
-        setattr(obj, self.sweep._Sweep__Systems[obj].sweepKey, sweep)
+    @property
+    def tProtocol(self):
+        return self.__tProtocol
 
+    @tProtocol.getter
+    def tProtocol(self, protoc):
+        self.__tProtocol = protoc
 
-        if self.qSys.Unitaries is None:
-            unitary = lio.Liouvillian(2 * np.pi * self.qSys.totalHam, timeStep=self.timeSweep.sweepPert)
-        else:
-            unitary = self.qSys.Unitaries(self.qSys, self.timeSweep.sweepPert)
+    def run(self, p, obj, sweepList):
+        if self.qSys._QuantumSystem__constructed == False:
+            self.qSys.constructCompSys()
 
-        state = self.qSys.initialState
-        if self.allStates:
-            states = [state]
-            for ii in range(len(self.times) - 1):
-                state = unitary @ state
-                states.append(state)
-            return states
-        else:
-            for ii in range(len(self.times) - 1):
-                state = unitary @ state
-            return state
-
-    def randStep(self, QSys, sweep):
-        if hasattr(QSys, self.sweepKey):
-            setattr(QSys, self.sweepKey, sweep)
-        elif hasattr(self, self.sweepKey):
-            setattr(self, self.sweepKey, sweep)
-        else:
-            print("no attribute")
-
-        UnitaryList = []
-        for stepSize in self.yData:
-            UnitaryList.append(self.qSys.Unitaries(self.qSys, stepSize))
-
-        state = self.qSys.initialState
-        if self.allStates:
-            states = [state]
-            for ijkn in range(len(UnitaryList)):
-                unitary = UnitaryList[ijkn]
-                state = unitary @ state
-                states.append(state)
-            return states
-        else:
-            for ijkn in range(UnitaryList):
-                unitary = UnitaryList[ijkn]
-                state = unitary @ state
-            return state
-
-    def evolveTimeDep(self, Qsys, sweep):
-        state = self.qSys.initialState
-        states = []
-        for value in sweep:
-            setattr(Qsys, self.timeKey, value)
-            unitary = lio.Liouvillian(2 * np.pi * self.qSys.totalHam, timeStep=self.StepSize)
-
-            state = unitary @ state
-            states.append(state)
-
+        states = p.map(partial(self.tProtocol, obj), sweepList)
         return states
-
-    def evolveTD_get_excitations(self, Qsys, sweep):
-        state = self.qSys.initialState
-        excitations_sweep = []
-
-        for value in sweep:
-            setattr(Qsys, self.timeKey, value)
-            unitary = lio.Liouvillian(2 * np.pi * self.qSys.totalHam, timeStep=self.StepSize)
-            state = unitary @ state
-
-            eigen_values, eigen_states = sp.linalg.eig(self.qSys.totalHam.A)
-
-            sort = np.argsort(eigen_values)
-            eigen_states = np.transpose(eigen_states.conj())[sort]
-
-            excitations = (np.abs(np.transpose(eigen_states @ state))**2)[0]
-            excitations_sweep.append(excitations)
-
-        return np.transpose(excitations_sweep)
 
