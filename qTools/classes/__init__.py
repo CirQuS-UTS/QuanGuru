ddbb9a23fd07fbf54c5956c8cbbfc4188d3f34d8
cahitkargi <13433634@student.uts.edu.au>
2020-02-15 19:38:33 +1100

    init modification

--- qTools/classes/__init__.py
+++ qTools/classes/__init__.py
@@ -1,3 +1,3 @@
-from .QSim import qSequence, Simulation, Sweep
+from .QSim import qSequence, Simulation, Sweep, sweep
 from .QSys import QuantumSystem, qCoupling, envCoupling, sysCoupling, qSystem, Qubit, Spin, Cavity
 from .QUni import qUniversal
