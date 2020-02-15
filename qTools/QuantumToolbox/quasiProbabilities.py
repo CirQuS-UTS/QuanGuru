2dbbab43219da30316f1c505ce3f00dbd609b73d
cahitkargi <13433634@student.uts.edu.au>
2020-02-15 19:38:16 +1100

    redundant namespaces removed

--- qTools/QuantumToolbox/quasiProbabilities.py
+++ qTools/QuantumToolbox/quasiProbabilities.py
@@ -1,7 +1,6 @@
 """Copied from Qutip"""
 
 import numpy as np
-import scipy.sparse as sp
 from scipy import (zeros, array, arange, exp, real, conj, pi,copy, sqrt, meshgrid, size, polyval, fliplr, conjugate)
 from scipy.special import factorial
 import scipy.linalg as la
