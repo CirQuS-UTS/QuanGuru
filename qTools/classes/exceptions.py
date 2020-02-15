8e36a85914c6f99d9eea640f4926f75b0450bfc5
cahitkargi <13433634@student.uts.edu.au>
2020-02-16 02:36:55 +1100

    an exception for qCoupling init

--- qTools/classes/exceptions.py
+++ qTools/classes/exceptions.py
@@ -1,3 +1,5 @@
+# TODO turn prints into actual error raise, they are print for testing
+
 def qSystemInitErrors(init):
     def new_function(obj, **kwargs):
         init(obj, **kwargs)
@@ -7,4 +9,22 @@ def qSystemInitErrors(init):
         elif obj.frequency is None:
             className = obj.__class__.__name__
             print(className + ' requires a frequency')
+    return new_function
+
+
+def qCouplingInitErrors(init):
+    def new_function(obj, *args):
+        init(obj, *args)
+        if obj._qCoupling__cFncs is None:
+            className = obj.__class__.__name__
+            print(className + ' requires a coupling functions')
+        elif obj._qCoupling__qSys is None:
+            className = obj.__class__.__name__
+            print(className + ' requires a coupling systems')
+        
+        for ind in range(len(obj._qCoupling__qSys)):
+            if len(obj._qCoupling__cFncs) != len(obj._qCoupling__qSys):
+                className = obj.__class__.__name__
+                print(className + ' requires same number of systems as coupling functions')
+
     return new_function
