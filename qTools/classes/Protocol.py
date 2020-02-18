3cc83ccae7cb93fe375c3e55db4557e2eb9f84f4
fabiolous <fabio.a.ferreirahenriques@student.uts.edu.au>
2020-02-18 15:13:47 +1100

    Start working on it

--- qTools/classes/Protocol.py
+++ qTools/classes/Protocol.py
@@ -3,49 +3,40 @@ import numpy as np
 # import scipy.sparse as sp
 # import qTools.QuantumToolbox.states as qSt
 # from qTools.classes.QSys import QuantumSystem
-from qTools.classes.QUni import qUniversal
+# from qTools.classes.QUni import qUniversal
 # from functools import partial
 # from qTools.classes.exceptions import sweepInitError
 """ under construction """
 
 
-class __Protocol(qUniversal):
-    def __init__(self, qSys, **kwargs):
-        super().__init__(**kwargs)
-        self.qSys = qSys
-        self._qUniversal__setKwargs(**kwargs)
-
-
-class Protocol(__Protocol):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class Protocol:
+    def __init__(self):
         self.steps = []
-        # maybe 
 
-    def add_step(self, step):
+    def add_step(self, qSystem=None, key=None, time=0, value=None):
+        step = Step(qSystem=None, key=None, time=0, value=None)
         self.steps.append(step)
+        step.superSys = self.superSys
+        return step
 
-    @property
     def unitary(self):
-        # need to find a way of not calculating this everytime it is called
-        # (this is solved by the way time evolution functions)
         unitary = self.steps[0].unitary
         for step in self.steps[1:]:
             unitary = unitary @ self.step.unitary
         return unitary
 
 
-class FreeEvolution(__Protocol):
-    def __init__(self, qSys, time, **kwargs):
-        super().__init__(qSys, **kwargs)
+class Step:
+    def __init__(self, qSystem, key, time=0, value=None):
+        self.qSystem = qSystem
+        self.key = key
+        self.value = value
         self.time = time
 
-    @property
     def unitary(self):
-        unitary = lio.Liouvillian(2 * np.pi * self.qSys.totalHam,
-                                  timeStep=self.time)
+        memory = getattr(self.qSystem, self.key)
+        setattr(self.qSystem, self.key, self.value)
+        unitary = lio.Liouvillian(
+            2 * np.pi * self.superSys.totalHam, timeStep=self.time)
+        setattr(self.qSystem, self.key, memory)
         return unitary
-
-
-class Gate(__Protocol):
-    pass
