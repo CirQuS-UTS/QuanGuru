c737e7dac4045150617d538b0f98ec849c53a5d8
fabiolous <fabio.a.ferreirahenriques@student.uts.edu.au>
2020-02-19 11:18:49 +1100

    Fixed Protocol and FreeEvolution classes

--- qTools/classes/Protocol.py
+++ qTools/classes/Protocol.py
@@ -1,42 +1,111 @@
 import qTools.QuantumToolbox.liouvillian as lio
+from qTools.classes.QUni import qUniversal
 import numpy as np
-# import scipy.sparse as sp
-# import qTools.QuantumToolbox.states as qSt
-# from qTools.classes.QSys import QuantumSystem
-# from qTools.classes.QUni import qUniversal
-# from functools import partial
-# from qTools.classes.exceptions import sweepInitError
 """ under construction """
 
 
-class Protocol:
-    def __init__(self):
+class Protocol(qUniversal):
+    __slots__  = ['steps']
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
         self.steps = []
+        self._qUniversal__setKwargs(**kwargs)
 
-    def add_step(self, qSystem=None, key=None, time=0, value=None):
-        step = Step(qSystem=None, key=None, time=0, value=None)
+    def add_step(self, step):
+        step.superSys = self
         self.steps.append(step)
-        step.superSys = self.superSys
-        return step
 
+    def free_evol(self, system=[], key=[], value=[], time=0):
+        free_evol = FreeEvolution(system=system, key=key, value=value, time=time, superSys=self)
+        self.steps.append(free_evol)
+        return  free_evol
+
+    @property
     def unitary(self):
         unitary = self.steps[0].unitary
         for step in self.steps[1:]:
-            unitary = unitary @ self.step.unitary
+            unitary = unitary @ step.unitary
         return unitary
 
+    #TODO: add a function which returns a schematic of the protocol
 
-class Step:
-    def __init__(self, qSystem, key, time=0, value=None):
-        self.qSystem = qSystem
-        self.key = key
-        self.value = value
-        self.time = time
 
+class FreeEvolution(qUniversal):
+    __slots__  = ['time', '__system', '__key', '__value']
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__system = []
+        self.__key = []
+        self.__value = []
+        self.time = 0
+        self._qUniversal__setKwargs(**kwargs)
+
+    # FIXME: There has to be a better way of guaranting the atributed are iterable!
+    @property
+    def system(self):
+        return self.__system
+    
+    @system.setter
+    def system(self, system):
+        if not hasattr(system, '__iter__'):
+            self.__system = [system]
+        else:
+            self.__system = system
+    
+    @system.getter
+    def system(self):
+        if len(self.__system)==1:
+            return self.__system[0]
+        else:
+            return self.__system
+
+    @property
+    def key(self):
+        return self.__key
+    
+    @key.setter
+    def key(self, key):
+        if not hasattr(key, '__iter__'):
+            self.__key = [key]
+        else:
+            self.__key = key
+
+    @key.getter
+    def key(self):
+        if len(self.__key)==1:
+            return self.__key[0]
+        else:
+            return self.__key
+
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, value):
+        if not hasattr(value, '__iter__'):
+            self.__value = [value]
+        else:
+            self.__value = value
+    
+    @value.getter
+    def value(self):
+        if len(self.__value)==1:
+            return self.__value[0]
+        else:
+            return self.__value
+
+    @property
     def unitary(self):
-        memory = getattr(self.qSystem, self.key)
-        setattr(self.qSystem, self.key, self.value)
+        memory = []
+        for ii, system in enumerate(self.system):
+            memory = getattr(system, self.key[ii])
+            setattr(system, self.key[ii], self.value[ii])
+
         unitary = lio.Liouvillian(
-            2 * np.pi * self.superSys.totalHam, timeStep=self.time)
-        setattr(self.qSystem, self.key, memory)
+            2 * np.pi * self.superSys.superSys.totalHam, timeStep=self.time)
+       
+        for ii, system in enumerate(self.system):
+            setattr(system, self.key[ii], memory[ii])
+
         return unitary
