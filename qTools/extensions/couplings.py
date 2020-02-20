d3779d0d5e50c0ffb1d9e7c0fea8502bcbc06418
fabiolous <fabio.a.ferreirahenriques@student.uts.edu.au>
2020-02-20 11:14:30 +1100

    Add Rabi coupling to couplings

--- qTools/extensions/couplings.py
+++ qTools/extensions/couplings.py
@@ -24,5 +24,27 @@ def JC(obj, subSys1, subSys2, cStrength):
     couplingObj.name = 'JCcoupling'
     return couplingObj
 
+def Rabi(obj, subSys1, subSys2, cStrength):
+    if isinstance(subSys1, Qubit):
+        if not isinstance(subSys2, Cavity):
+            raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
+        qsystems = [subSys2, subSys1]
+    elif isinstance(subSys2, Qubit):
+        if not isinstance(subSys1, Cavity):
+            raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
+        qsystems = [subSys1, subSys2]
+
+    obj.couplingName = 'Rabi'
+    if qsystems[1].operator == sigmaz:
+        print('sigmaz')
+        couplingObj = obj.createSysCoupling(qsystems, [destroy, sigmax], superSys=obj, couplingStrength=cStrength)
+        couplingObj.addTerm(qsystems,[create, sigmax])
+    # else:
+    #     print('number')
+    #     couplingObj = obj.createSysCoupling(qsystems, [destroy, create], superSys=obj, couplingStrength=cStrength)
+    #     couplingObj.addTerm(qsystems,[create, destroy])
+    # couplingObj.name = 'JCcoupling'
+    return couplingObj
 
 QuantumSystem.JC = JC
+QuantumSystem.Rabi = Rabi
