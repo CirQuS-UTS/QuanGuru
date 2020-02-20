b29e115f1aa4c0d783024aa18643402b8cdaa5a5
fabiolous <fabio.a.ferreirahenriques@student.uts.edu.au>
2020-02-20 13:15:12 +1100

    Fix small bug

--- qTools/extensions/couplings.py
+++ qTools/extensions/couplings.py
@@ -1,5 +1,5 @@
 from qTools.classes import QuantumSystem, Qubit, Cavity
-from qTools.QuantumToolbox.operators import sigmaz, number, sigmam, sigmap, create, destroy
+from qTools.QuantumToolbox.operators import sigmaz, number, sigmam, sigmap, create, destroy, sigmax
 
 
 def JC(obj, subSys1, subSys2, cStrength):
