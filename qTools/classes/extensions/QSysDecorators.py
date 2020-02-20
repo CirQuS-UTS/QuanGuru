a5b4f02172cdeabf70b642951cc3e9aa77bde52b
cahitkargi <13433634@student.uts.edu.au>
2020-02-20 19:35:57 +1100

    two new decorator extensions for QSys

--- qTools/classes/extensions/QSysDecorators.py
+++ qTools/classes/extensions/QSysDecorators.py
@@ -1,18 +1,48 @@
 import scipy.sparse as sp
+from qTools.classes.QUni import qUniversal
 
 
 def asignState(stateCreationFunc):
     def InitialStateDecorator(initialState):
         def wrapper(obj, inp):
+            print(inp)
             if sp.issparse(inp):
                 if inp.shape[0] == obj.dimension:
                     obj._genericQSys__initialState = inp
+                    print('here3')
                 else:
                     print('dude')
             elif isinstance(inp, int):
                 obj._genericQSys__initialState = stateCreationFunc(obj.dimension, inp)
-            elif len(inp) == len(obj.subSystems):
-                dims = [val.dimension for val in obj.subSystems.values()]
+            elif len(inp) == len(obj.qSystems):
+                print('creating state')
+                dims = [val.dimension for val in obj.qSystems.values()]
                 obj._genericQSys__initialState = stateCreationFunc(dims, inp)
+            print('here2')
         return wrapper
     return InitialStateDecorator
+
+
+def addCreateInstance(functionToCall):
+    def systemAddCreateDecorator(addCreate):
+        def wrapper(obj, clsInst, *args, **kwargs):
+            newSub = None
+            if isinstance(clsInst, qUniversal):
+                if clsInst.superSys is None:
+                    newSub = functionToCall(obj, clsInst)
+                elif clsInst.superSys is obj:
+                    clasOfNew = clsInst.__class__
+                    newSub = clasOfNew.createCopy(clsInst, *args)
+                    newSub = functionToCall(obj, newSub, *args)
+                    print('Sub system is already in the composite, copy created and added')
+            elif isinstance(clsInst, list):
+                newSub = functionToCall(obj, 'qCoupling', clsInst, *args, **kwargs)
+            else:
+                newSub = clsInst(*args, **kwargs)
+                newSub =functionToCall(obj, newSub, *args)
+            newSub._qUniversal__setKwargs(**kwargs)
+            newSub.superSys = obj
+            obj.subSystems[newSub.name] = newSub
+            return newSub
+        return wrapper
+    return systemAddCreateDecorator
