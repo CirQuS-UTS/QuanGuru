288a0f884dddb63c1bf010d5d72f78df6271c7e8
cahitkargi <13433634@student.uts.edu.au>
2020-03-05 02:14:59 +1100

    all timeEvols that do not save states are in new modular evolve

--- qTools/classes/extensions/modularSweep.py
+++ qTools/classes/extensions/modularSweep.py
@@ -1,13 +1,68 @@
-import qTools.QuantumToolbox.liouvillian as lio
-from qTools.classes.QUni import qUniversal
 from functools import partial
 import numpy as np
-from copy import deepcopy
 import datetime
-import matplotlib.pyplot as plt
 
 
-def modularSweep(ind, *args):
+def runSimulation(qSim, p):
+    '''if qSim.delState is False:
+        if len(qSim.Loop.sweeps) > 0:
+            if len(qSim.whileLoop.sweeps) > 0:
+                if p is None:
+                    qSim.qRes.indB = 0
+                    withLWnp(qSim)
+                else:
+                    qSim.qRes.indB = 0
+                    withLWp(qSim, p)
+            else:
+                if p is None:
+                    qSim.qRes.indB = 0
+                    withLOnp(qSim)
+                else:
+                    qSim.qRes.indB = 0
+                    withLOp(qSim, p)
+        else:
+            if len(qSim.whileLoop.sweeps) > 0:
+                qSim.qRes.indB = 0
+                qSim.qRes.indL = 0
+                withW(qSim)
+            else:
+                for qSys in qSim.subSys.values():
+                    qSys._genericQSys__prepareLastStateList()
+                unitary = exponUni(qSim)
+                qSim.qRes.indB = 0
+                qSim.qRes.indL = 0
+                __timeEvol(qSim, unitary)
+    else:'''
+    if len(qSim.Loop.sweeps) > 0:
+        if len(qSim.whileLoop.sweeps) > 0:
+            if p is None:
+                qSim.qRes.indB = 0
+                withLWnpDel(qSim)
+            else:
+                qSim.qRes.indB = 0
+                withLWpDel(qSim, p)
+        else:
+            if p is None:
+                qSim.qRes.indB = 0
+                withLOnpDel(qSim)
+            else:
+                qSim.qRes.indB = 0
+                withLOpDel(qSim, p)
+    else:
+        if len(qSim.whileLoop.sweeps) > 0:
+            qSim.qRes.indB = 0
+            qSim.qRes.indL = 0
+            withWDel(qSim)
+        else:
+            for qSys in qSim.subSys.values():
+                qSys._genericQSys__prepareLastStateList()
+            unitary = exponUni(qSim)
+            qSim.qRes.indB = 0
+            qSim.qRes.indL = 0
+            __timeEvolDel(qSim, unitary)
+
+
+def indicesForSweep(ind, *args):
     remain = 0
     indices = []
     for arg in args:
@@ -18,21 +73,50 @@ def modularSweep(ind, *args):
     return indices
 
 
+def withLWnpDel(qSim):
+    results = []
+    for ind in range(len(qSim.Loop.sweeps[0].sweepList)):
+        indices = indicesForSweep(ind, *qSim.inds)
+        runSequence(qSim.Loop, ind)
+        qSim.qRes.resetLast()
+        withWDel(qSim)
+        results.append(qSim.qRes.results)
+
+def withLOnpDel(qSim):
+    results = []
+    for ind in range(len(qSim.Loop.sweeps[0].sweepList)):
+        indices = indicesForSweep(ind, *qSim.inds)
+        runSequence(qSim.Loop, ind)
+        for qSys in qSim.subSys.values():
+            qSys._genericQSys__prepareLastStateList()
+        unitary = exponUni(qSim)
+        qSim.qRes.resetLast()
+        for ii in range(qSim.steps):
+            __timeEvolDel(qSim, unitary)
+        results.append(qSim.qRes.results)
+
+def withLWpDel(qSim, p):
+    results = p.map(partial(parallelSequenceWDel, qSim), range(len(qSim.Loop.sweeps[0].sweepList)))
+    qSim.qRes._organiseRes(results, qSim.inds, qSim.indMultip)
+            
+
+def parallelSequenceWDel(qSim, ind):
+    indices = indicesForSweep(ind, *qSim.inds)
+    runSequence(qSim.Loop, indices)
+    qSim.qRes.resetLast()
+    withWDel(qSim)
+    return qSim.qRes.results
+
+
 def withLOpDel(qSim, p):
     nw = datetime.datetime.now()
     results = p.map(partial(parallelSequenceODel, qSim), range(qSim.indMultip))
     en = datetime.datetime.now()
-
-    for res in results:
-        for key, val in res.items():
-            qSim.qRes._qResults__results[key].append(val)
-    
-    for key, val in qSim.qRes._qResults__results.items():
-        qSim.qRes._qResults__results[key] = np.reshape(val, (*list(reversed(qSim.inds)),qSim.steps,))
+    qSim.qRes._organiseRes(results, qSim.inds, qSim.indMultip)
 
 
 def parallelSequenceODel(qSim, ind):
-    indices = modularSweep(ind, *qSim.inds)
+    indices = indicesForSweep(ind, *qSim.inds)
     runSequence(qSim.Loop, indices)
     for qSys in qSim.subSys.values():
         qSys._genericQSys__prepareLastStateList()
@@ -41,6 +125,15 @@ def parallelSequenceODel(qSim, ind):
     for ii in range(qSim.steps):
         __timeEvolDel(qSim, unitary)
     return qSim.qRes.results
+
+def withWDel(qSim):
+    for qSys in qSim.subSys.values():
+        qSys._genericQSys__prepareLastStateList()
+
+    for ind in range(len(qSim.whileLoop.sweeps[0].sweepList)):
+        runSequence(qSim.whileLoop, ind)
+        unitary = exponUni(qSim)
+        __timeEvolDel(qSim, unitary)
 
 def __timeEvolDel(qSim, unitaryList):
     for ii in range(qSim.samples):
