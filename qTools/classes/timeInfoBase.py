0a5786fd22643f2a0db2f526613ec84efcec0b01
cahitkargi <13433634@student.uts.edu.au>
2020-03-08 20:34:42 +1100

    bug fox in new timeBase class

--- qTools/classes/timeInfoBase.py
+++ qTools/classes/timeInfoBase.py
@@ -1,11 +1,12 @@
 from qTools.classes.QUni import qUniversal
+from qTools.classes.QResDict import qResults
 
 class timeBase(qUniversal):
     instances = 0
     label = 'timeBase'
     
-    __slots__ = ['delStates', '__finalTime', '__stepSize', '__samples', '__step', 'compute', 'calculate']
-    # TODO Same as previous 
+    __slots__ = ['delStates', '__finalTime', '__stepSize', '__samples', '__step', 'compute', 'calculate', 'qRes']
+    
     def __init__(self, **kwargs):
         super().__init__()
 
@@ -20,43 +21,44 @@ class timeBase(qUniversal):
         self.calculate = None
 
         self._qUniversal__setKwargs(**kwargs)
-
+        self.qRes = qResults(superSys=self)
+        
     @property
     def finalTime(self):
-        return self._Simulation__finalTime
+        return self._timeBase__finalTime
 
     @finalTime.setter
     def finalTime(self, fTime):
-        self._Simulation__finalTime = fTime
+        self._timeBase__finalTime = fTime
         if self.stepSize is not None:
-            self._Simulation__step = int((fTime//self.stepSize) + 1)
+            self._timeBase__step = int((fTime//self.stepSize) + 1)
 
     @property
     def steps(self):
         if self.finalTime is None:
-            self._Simulation__finalTime = self._Simulation__step * self.stepSize
+            self._timeBase__finalTime = self._timeBase__step * self.stepSize
         return int((self.finalTime//self.stepSize) + 1)
 
     @steps.setter
     def steps(self, num):
-        self._Simulation__step = num
+        self._timeBase__step = num
         if self.finalTime is not None:
-            self._Simulation__stepSize = self.finalTime/num
+            self._timeBase__stepSize = self.finalTime/num
 
     @property
     def stepSize(self):
-        return self._Simulation__stepSize
+        return self._timeBase__stepSize
 
     @stepSize.setter
     def stepSize(self, stepsize):
-        self._Simulation__stepSize = stepsize
+        self._timeBase__stepSize = stepsize
         if self.finalTime is not None:
-            self._Simulation__step = int((self.finalTime//stepsize) + 1)
+            self._timeBase__step = int((self.finalTime//stepsize) + 1)
 
     @property
     def samples(self):
-        return self._Simulation__samples
+        return self._timeBase__samples
 
     @samples.setter
     def samples(self, num):
-        self._Simulation__samples = num
+        self._timeBase__samples = num
