32895ab81618a830da093c8014eb07eb31b73499
cahitkargi <13433634@student.uts.edu.au>
2020-03-25 18:44:33 +1100

    bug fix and some changes in modularSweeep

--- qTools/classes/extensions/modularSweep.py
+++ qTools/classes/extensions/modularSweep.py
@@ -11,10 +11,10 @@ def runSimulation(qSim, p):
         if len(qSim.timeDependency.sweeps) > 0:
             timeDependent(qSim)
         else:
-            #timeEvolBase(qSim, qSim.steps)
             qSim.evolFunc(qSim)
 
 
+# function used in modular sweep
 def indicesForSweep(ind, *args):
     remain = 0
     indices = []
@@ -26,6 +26,7 @@ def indicesForSweep(ind, *args):
     return indices
 
 
+# This is the single process function
 def nonParalEvol(qSim):
     if len(qSim.timeDependency.sweeps) > 0:
         evolFunc = timeDependent
@@ -38,42 +39,45 @@ def nonParalEvol(qSim):
     qSim.qRes._finaliseAll(qSim.Sweep.inds)
 
 
+# multi-processing functions
 def paralEvol(qSim, p):
     if len(qSim.timeDependency.sweeps) > 0:
-        results = p.map(partial(partial(parallelTimeEvol, qSim), timeDependent),range(qSim.Sweep.indMultip))
+        results = p.map(partial(partial(parallelTimeEvol, qSim), timeDependent), range(qSim.Sweep.indMultip))
     else:
-        results = p.map(partial(partial(parallelTimeEvol, qSim), timeEvolBase),range(qSim.Sweep.indMultip))
+        results = p.map(partial(partial(parallelTimeEvol, qSim), qSim.evolFunc), range(qSim.Sweep.indMultip))
     qSim.qRes._organiseMultiProcRes(results, qSim.Sweep.inds)
 
 def parallelTimeEvol(qSim, evolFunc, ind):
     _runSweepAndPrep(qSim, ind, evolFunc)
     return deepcopy(qSim.qRes.allResults)
 
+
+# These two functions, respectively, run Sweep and timeDependent (sweep) parameter updates
+# In the timeDependet case, evolFunc of first fuctioon is the second function
 def _runSweepAndPrep(qSim, ind, evolFunc):
     qSim.Sweep.runSweep(indicesForSweep(ind, *qSim.Sweep.inds))
     for protoc, qSys in qSim.subSys.items():
         protoc.lastState = qSys.initialState
     qSim.qRes._resetLast()
-    evolFunc(qSim, qSim.steps)
+    evolFunc(qSim)
 
-def timeDependent(qSim, steps):
+def timeDependent(qSim):
     qSim.timeDependency.prepare()
     for ind in range(qSim.timeDependency.indMultip):
         qSim.timeDependency.runSweep(indicesForSweep(ind, *qSim.timeDependency.inds))
-        #exponUni(qSim)
-        #timeEvolBase(qSim)
-        qSim.evolFunc(qSim, 1)
-
-'''def _timeEvol(qSim):
-    qSim.evolFunc(qSim, qSim.steps)'''
+        qSim._timeBase__step = 1
+        qSim.evolFunc(qSim)
         
+
+# These two are the specific solution method, user should define their own timeEvol function to use other solution methods
+# This flexibility should be reflected into protocol object
 def exponUni(qSim):
     for protocol in qSim.subSys.keys():
         protocol.createUnitary()
 
-def timeEvolBase(qSim, steps):
+def timeEvolBase(qSim):
     exponUni(qSim)
-    for ii in range(steps):
+    for ii in range(qSim._timeBase__step):
         for protocol in qSim.subSys.keys():
             qSim._Simulation__compute()
             for ii in range(protocol.samples):
