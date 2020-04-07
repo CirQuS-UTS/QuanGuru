f9cb9c469a3489883e4b8fe850b9f5488093995b
cahitkargi <13433634@student.uts.edu.au>
2020-04-07 13:21:20 +1000

    timBase property setters and getters modified

--- qTools/classes/timeBase.py
+++ qTools/classes/timeBase.py
@@ -4,11 +4,11 @@ class timeBase(computeBase):
     instances = 0
     label = 'timeBase'
     
-    __slots__ = ['__finalTime', '__stepSize', '__samples', '__step', '__bound']
+    __slots__ = ['__finalTime', '__stepSize', '__samples', '__step', '__bound', '__paramUpdated']
     
     def __init__(self, **kwargs):
         super().__init__(name=kwargs.pop('name', None))
-
+        self.__paramUpdated = True
         self.__finalTime = None
         self.__stepSize = None
         self.__samples = None
@@ -18,11 +18,30 @@ class timeBase(computeBase):
         self._qUniversal__setKwargs(**kwargs)
 
     @property
+    def _paramUpdated(self):
+        if self.bound is not self:
+            return self.bound._paramUpdated
+        else:
+            return self._timeBase__paramUpdated
+
+    @_paramUpdated.setter
+    def _paramUpdated(self, boolean): 
+        self._timeBase__paramUpdated = boolean
+
+    @property
+    def bound(self):
+        return self._timeBase__bound
+
+    @property
     def finalTime(self):
-        return self._timeBase__finalTime
+        if self.bound is not self:
+            return self.bound.finalTime
+        else:
+            return self._timeBase__finalTime
 
     @finalTime.setter
     def finalTime(self, fTime):
+        self._paramUpdated = True
         self._timeBase__finalTime = fTime
         if self.stepSize is not None:
             self._timeBase__step = int((fTime//self.stepSize) + 1)
@@ -35,16 +54,21 @@ class timeBase(computeBase):
 
     @steps.setter
     def steps(self, num):
+        self._paramUpdated = True
         self._timeBase__step = num
         if self.finalTime is not None:
             self._timeBase__stepSize = self.finalTime/num
 
     @property
     def stepSize(self):
-        return self._timeBase__stepSize
+        if self.bound is not self:
+            return self.bound.stepSize
+        else:
+            return self._timeBase__stepSize
 
     @stepSize.setter
     def stepSize(self, stepsize):
+        self._paramUpdated = True
         self._timeBase__stepSize = stepsize
         if self.finalTime is not None:
             self._timeBase__step = int((self.finalTime//stepsize) + 1)
@@ -55,6 +79,7 @@ class timeBase(computeBase):
 
     @samples.setter
     def samples(self, num):
+        self._paramUpdated = True
         self._timeBase__samples = num
 
     def prepare(self, obj):
