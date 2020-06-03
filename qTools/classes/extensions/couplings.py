e488aea3a570c3bea02575ff776c16a787a4c26e
cahitkargi <13433634@student.uts.edu.au>
2020-06-03 10:03:28 +1000

    coupling extensions improved by a decorator to cover more cases

--- qTools/classes/extensions/couplings.py
+++ qTools/classes/extensions/couplings.py
@@ -2,57 +2,61 @@ from qTools.classes import QuantumSystem, Qubit, Cavity
 from qTools.QuantumToolbox.operators import sigmaz, sigmam, sigmap, create, destroy, sigmax
 
 
-def JC(obj, subSys1, subSys2, cStrength):
-    if isinstance(subSys1, Qubit):
-        if not isinstance(subSys2, Cavity):
-            raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
-        qsystems = [subSys2, subSys1]
-    elif isinstance(subSys2, Qubit):
-        if not isinstance(subSys1, Cavity):
-            raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
-        qsystems = [subSys1, subSys2]
+def checkCavQub(coupler):
+    def couplerDecorator(obj, couplingStrength, subSys1=None, subSys2=None):
+        if isinstance(subSys1, Qubit):
+            if not isinstance(subSys2, Cavity):
+                raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
+            qsystems = [subSys2, subSys1]
+        elif isinstance(subSys2, Qubit):
+            if not isinstance(subSys1, Cavity):
+                raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
+            qsystems = [subSys1, subSys2]
+        elif ((subSys1 is None) or (subSys2 is None)):
+            qsystems = []
+            for sys in obj.subSys.values():
+                if isinstance(sys, (Cavity, Qubit)):
+                    if len(qsystems) == 0:
+                        qsystems.append(sys)
+                    elif not isinstance(sys, qsystems[0].__class__):
+                        qsystems.append(sys)
 
+                    if len(qsystems) == 2:
+                        break
+            return couplerDecorator(obj, couplingStrength, *qsystems)
+        return coupler(obj, couplingStrength, *qsystems)
+    return couplerDecorator
+
+
+@checkCavQub
+def JC(obj, couplingStrength, subSys1=None, subSys2=None):
+    qsystems = [subSys1, subSys2]
     obj.couplingName = 'JC'
     if qsystems[1].operator == sigmaz: # pylint: disable=comparison-with-callable
-        #print('sigmaz')
         couplingObj = obj.createSysCoupling(qsystems, [destroy, sigmap], qsystems,
-                                            [create, sigmam], superSys=obj, couplingStrength=cStrength)
-        #couplingObj.addTerm()
+                                            [create, sigmam], superSys=obj, couplingStrength=couplingStrength)
     else:
-        #print('number')
-        couplingObj = obj.createSysCoupling(qsystems, [destroy, create], superSys=obj, couplingStrength=cStrength)
+        couplingObj = obj.createSysCoupling(qsystems, [destroy, create], superSys=obj,
+                                            couplingStrength=couplingStrength)
         couplingObj.addTerm(qsystems, [create, destroy])
     couplingObj.name = 'JCcoupling'
     return couplingObj
 
-def Rabi(obj, subSys1, subSys2, cStrength):
-    if isinstance(subSys1, Qubit):
-        if not isinstance(subSys2, Cavity):
-            raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
-        qsystems = [subSys2, subSys1]
-    elif isinstance(subSys2, Qubit):
-        if not isinstance(subSys1, Cavity):
-            raise ValueError('Jaynes-Cummings requires a qubit and a cavity')
-        qsystems = [subSys1, subSys2]
 
+@checkCavQub
+def Rabi(obj, couplingStrength, subSys1=None, subSys2=None):
+    qsystems = [subSys1, subSys2]
     obj.couplingName = 'Rabi'
     if qsystems[1].operator == sigmaz: # pylint: disable=comparison-with-callable
         couplingObj = obj.createSysCoupling(qsystems, [destroy, sigmax], qsystems,
-                                            [create, sigmax], superSys=obj, couplingStrength=cStrength)
+                                            [create, sigmax], superSys=obj, couplingStrength=couplingStrength)
         couplingObj.addTerm()
     # else:
     #     print('number')
-    #     couplingObj = obj.createSysCoupling(qsystems, [destroy, create], superSys=obj, couplingStrength=cStrength)
+    #     couplingObj = obj.createSysCoupling(qsystems, [destroy, create], superSys=obj, couplingStrength=couplingStrength)
     #     couplingObj.addTerm(qsystems,[create, destroy])
     # couplingObj.name = 'JCcoupling'
     return couplingObj
 
 QuantumSystem.JC = JC
 QuantumSystem.Rabi = Rabi
-
-
-def qDrive(obj):
-    pass
-
-Qubit.drive = qDrive
-
