c66ac318e591a444613bbbfb0b4006569b57f22c
cahit kargı <cahitkargi@cahits-MacBook-Pro.local>
2020-06-04 10:18:47 +1000

    genericQSys __radd__ enabled

--- qTools/classes/QSys.py
+++ qTools/classes/QSys.py
@@ -33,23 +33,19 @@ class genericQSys(qBaseSim):
             newComp = other
         return newComp
 
-    # def __radd__(self, other):
-    #     if isinstance(self, QuantumSystem) and isinstance(other, qSystem):
-    #         self.addSubSys(qSystem)
-    #         newComp = self
-    #     elif ((isinstance(self, qSystem) and isinstance(other, qSystem)) or
-    #          (isinstance(self, QuantumSystem) and isinstance(other, QuantumSystem))):
-    #         newComp = QuantumSystem()
-    #         newComp.addSubSys(other)
-    #         newComp.addSubSys(self)
-    #     elif isinstance(self, qSystem) and isinstance(other, QuantumSystem):
-    #         other.addSubSys(self)
-    #         newComp = other
-
-    #     newComp = QuantumSystem()
-    #     newComp.addSubSys(other)
-    #     newComp.addSubSys(self)
-    #     return newComp
+    def __radd__(self, other):
+        if isinstance(self, QuantumSystem) and isinstance(other, qSystem):
+            self.addSubSys(qSystem)
+            newComp = self
+        elif ((isinstance(self, qSystem) and isinstance(other, qSystem)) or
+             (isinstance(self, QuantumSystem) and isinstance(other, QuantumSystem))):
+            newComp = QuantumSystem()
+            newComp.addSubSys(other)
+            newComp.addSubSys(self)
+        elif isinstance(self, qSystem) and isinstance(other, QuantumSystem):
+            other.addSubSys(self)
+            newComp = other
+        return newComp
 
     def __init__(self, **kwargs):
         super().__init__()
