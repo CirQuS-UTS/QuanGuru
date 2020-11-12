850efad107670aba78198817aa4dce0064c9494c
JP Dehollain <jpdehollain@users.noreply.github.com>
2020-11-12 13:20:41 +1100

    parametrized all test functions

--- tests/test_states.py
+++ tests/test_states.py
@@ -1,47 +1,63 @@
+import pytest
 import numpy as np
 from qTools.QuantumToolbox import states #pylint: disable=import-error
 
-def test_basis():
-    testSparse = np.array([[0],[0],[0],[1],[0],[0],[0],[0],[0],[0],[0],[0],[0]])
-    testArray = np.array([[0],[0],[0],[0],[0],[0],[1]])
-    psiSparse = states.basis(13, 3, sparse=True)
-    psiArray = states.basis(7, 6, sparse=False)
-    assert (psiSparse.A == testSparse).all() and (psiArray == testArray).all()
+@pytest.mark.parametrize("params,expected", [
+    ((8,3,True), np.array([[0],[0],[0],[1],[0],[0],[0],[0]])),
+    ((7,6,False), np.array([[0],[0],[0],[0],[0],[0],[1]]))
+    ])
+def test_basis(params,expected):
+    psi = states.basis(*params)
+    if not isinstance(psi, np.ndarray):
+        psi = psi.A
+    assert (psi == expected).all()
 
-def test_completeBasis():
-    testSparse = [np.array([[1],[0],[0],[0],[0]]),
-                  np.array([[0],[1],[0],[0],[0]]),
-                  np.array([[0],[0],[1],[0],[0]]),
-                  np.array([[0],[0],[0],[1],[0]]),
-                  np.array([[0],[0],[0],[0],[1]])]
-    testArray = [np.array([[1],[0],[0]]),
-                np.array([[0],[1],[0]]),
-                np.array([[0],[0],[1]])]
-    psiSparse = states.completeBasis(5, sparse=True)
-    psiArray = states.completeBasis(3, sparse=False)
-    assert all([(p.A == t).all() for p,t in zip(psiSparse,testSparse)]) and \
-           all([(p == t).all() for p,t in zip(psiArray,testArray)])
+@pytest.mark.parametrize("params,expected", [
+    ((5,True), [np.array([[1],[0],[0],[0],[0]]),
+                np.array([[0],[1],[0],[0],[0]]),
+                np.array([[0],[0],[1],[0],[0]]),
+                np.array([[0],[0],[0],[1],[0]]),
+                np.array([[0],[0],[0],[0],[1]])]),
+    ((3,False), [np.array([[1],[0],[0]]),
+                 np.array([[0],[1],[0]]),
+                 np.array([[0],[0],[1]])])
+    ])
+def test_completeBasis(params,expected):
+    psiList = states.completeBasis(*params)
+    for i,p in enumerate(psiList):
+        if not isinstance(p, np.ndarray):
+            psiList[i] = p.A
+    assert all([(p == e).all() for p,e in zip(psiList,expected)])
 
-def test_basisBra():
-    testSparse = np.array([[1,0,0,0,0,0]])
-    testArray = np.array([[0,0,0,0,0,0,0,1,0]])
-    psiSparse = states.basisBra(6, 0, sparse=True)
-    psiArray = states.basisBra(9, 7, sparse=False)
-    assert (psiSparse.A == testSparse).all() and (psiArray == testArray).all()
+@pytest.mark.parametrize("params,expected", [
+    ((6,0,True), np.array([[1,0,0,0,0,0]])),
+    ((9,7,False), np.array([[0,0,0,0,0,0,0,1,0]]))
+    ])
+def test_basisBra(params,expected):
+    psiBra = states.basisBra(*params)
+    if not isinstance(psiBra, np.ndarray):
+        psiBra = psiBra.A
+    assert (psiBra == expected).all()
 
 def test_superPos():
     assert True is False
 
-def test_outerProd():
-    testOP1 = np.array([[0.5,0.5],[0.5,0.5]])
-    testOP2 = np.array([[0.707,0],[0.707,0]])
-    rho1 = states.outerProd(states.superPos(2,[0,1]))
-    rho2 = states.outerProd(states.superPos(2,[0,1]),states.basis(2,0))
-    assert (np.around(rho1.A,3)==testOP1).all() and (np.around(rho2.A,3)==testOP2).all()
+@pytest.mark.parametrize("params,expected", [
+    ((states.superPos(2,[0,1])), np.array([[0.5,0.5],[0.5,0.5]])),
+    ((states.superPos(2,[0,1]),states.basis(2,0)), np.array([[0.707,0],[0.707,0]]))
+    ])
+def test_outerProd(params,expected):
+    rho = states.outerProd(*params)
+    if not isinstance(rho, np.ndarray):
+        rho = rho.A
+    assert (np.around(rho,3)==expected).all()
 
-def test_densityMatrix():
-    testDM1 = np.array([[0.5,0.5],[0.5,0.5]])
-    testDM2 = np.array([[0.75,0.25],[0.25,0.25]])
-    rho1 = states.densityMatrix(states.superPos(2,[0,1]))
-    rho2 = states.densityMatrix([states.superPos(2,[0,1]),states.basis(2,0)],[0.5,0.5])
-    assert (np.around(rho1.A,3)==testDM1).all() and (np.around(rho2.A,3)==testDM2).all()
+@pytest.mark.parametrize("params,expected", [
+    ((states.superPos(2,[0,1])), np.array([[0.5,0.5],[0.5,0.5]])),
+    (([states.superPos(2,[0,1]),states.basis(2,0)],[0.5,0.5]), np.array([[0.75,0.25],[0.25,0.25]]))
+    ])
+def test_densityMatrix(params,expected):
+    rho = states.densityMatrix(*params)
+    if not isinstance(rho, np.ndarray):
+        rho = rho.A
+    assert (np.around(rho,3)==expected).all()
