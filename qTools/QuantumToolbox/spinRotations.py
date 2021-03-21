bug fixes and re-structuring rotation and xGate

qTools/QuantumToolbox/spinRotations.py
@@ -17,7 +17,8 @@ r"""
 
 import numpy as np # type: ignore
 
-from .operators import sigmaz, sigmax, sigmay, identity
+from .operators import sigmaz, sigmax, sigmay, identity, Jz, Jx, Jy
+from .evolution import Unitary
 from .customTypes import Matrix
 
 
@@ -50,7 +51,7 @@ def xRotation(angle: float, sparse: bool = True) -> Matrix:
     --------
     # TODO
     """
-
+    angle = angle/2
     rotUnitary = round(np.cos(angle), 15)*identity(2, sparse) - 1j*round(np.sin(angle), 15)*sigmax(sparse=sparse)
     return rotUnitary
 
@@ -75,7 +76,7 @@ def yRotation(angle: float, sparse: bool = True) -> Matrix:
     --------
     # TODO
     """
-
+    angle = angle/2
     rotUnitary = round(np.cos(angle), 15)*identity(2, sparse) - 1j*round(np.sin(angle), 15)*sigmay(sparse=sparse)
     return rotUnitary
 
@@ -100,14 +101,15 @@ def zRotation(angle: float, sparse: bool = True) -> Matrix:
     --------
     # TODO
     """
-
+    angle = angle/2
     rotUnitary = round(np.cos(angle), 15)*identity(2, sparse) - 1j*round(np.sin(angle), 15)*sigmaz(sparse=sparse)
     return rotUnitary
 
 
 def qubRotation(xyz: str, angle: float, sparse: bool = True) -> Matrix:
     r"""
-    Creates the operator :math:`R_{i}(\theta)` for qubit rotation around the i = x/y/z-axis by `angle` :math:`\theta`.
+    Creates the operator :math:`R_{i}(\theta) := e^{-i\sigma_{i}\theta/2}` for qubit rotation around the i = x/y/z-axis
+    by `angle` :math:`\theta`.
 
     Parameters
     ----------
@@ -136,4 +138,43 @@ def qubRotation(xyz: str, angle: float, sparse: bool = True) -> Matrix:
         rotUnitary = zRotation(angle, sparse)
     else:
         raise ValueError(xyz + ' is not supported')
+    return rotUnitary
+
+def spinRotation(xyz: str, angle: float, j: float, sparse: bool = True, isDim: bool = False) -> Matrix:
+    r"""
+    Creates the operator :math:`R_{i}(\theta) := e^{-iJ_{i}\theta}` for a spin value j rotation around i = x/y/z-axis
+    by `angle` :math:`\theta`.
+
+    Parameters
+    ----------
+    xyz : str
+        string for rotation direction x, y, or z
+    angle : float
+        angle of rotation
+    j : int or float
+        integer or half-integer spin quantum number, or the dimension (then spin quantum number = (d-1)/2)
+    sparse : bool
+        if True(False), the returned Matrix type will be sparse(array)
+    isDim : bool
+        boolean for whether j is spin quantum number of dimension
+
+    Returns
+    -------
+    Matrix
+        Spin x/y/z rotation operator.
+
+    Examples
+    --------
+    # TODO
+    """
+    if xyz.lower() == 'x':
+        rotOp = Jx
+    elif xyz.lower() == 'y':
+        rotOp = Jy
+    elif xyz.lower() == 'z':
+        rotOp = Jz
+    else:
+        raise ValueError(xyz + ' is not supported')
+    rotOp = rotOp(j, isDim=isDim, sparse=sparse)
+    rotUnitary = Unitary(angle*rotOp)
     return rotUnitary

qTools/classes/QGates.py
@@ -1,10 +1,66 @@
-import numpy as np
 from .QPro import Gate
+from .baseClasses import setAttr
 from ..QuantumToolbox import evolution
 from ..QuantumToolbox import operators #pylint: disable=relative-beyond-top-level
-from ..QuantumToolbox import qubitRotations #pylint: disable=relative-beyond-top-level
+from ..QuantumToolbox import spinRotations #pylint: disable=relative-beyond-top-level
 
-class xGate(Gate): # pylint: disable=too-many-ancestors
+class SpinRotation(Gate): # pylint: disable=too-many-ancestors
+    label = 'SpinRotation'
+    #: (**class attribute**) number of instances created internally by the library
+    _internalInstances: int = 0
+    #: (**class attribute**) number of instances created explicitly by the user
+    _externalInstances: int = 0
+    #: (**class attribute**) number of total instances = _internalInstances + _externalInstances
+    _instances: int = 0
+    __slots__ = ['__angle', '__rotationAxis', 'phase', '_rotationOp']
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.__angle = None
+        self.__rotationAxis = None
+        self._rotationOp = None
+        self.phase = 1
+        #self._createUnitary = self._rotMat
+        self._named__setKwargs(**kwargs) # pylint: disable=no-member
+
+    @property
+    def angle(self):
+        return self._SpinRotation__angle
+
+    @angle.setter
+    def angle(self, val):
+        setAttr(self, '_SpinRotation__angle', val)
+
+    @property
+    def rotationAxis(self):
+        return self._SpinRotation__rotationAxis # pylint: disable=no-member
+
+    @rotationAxis.setter
+    def rotationAxis(self, axStr):
+        setAttr(self, '_SpinRotation__rotationAxis', axStr)
+        if axStr.lower() == 'x':
+            self._rotationOp = operators.Jx
+        elif axStr.lower() == 'y':
+            self._rotationOp = operators.Jy
+        elif axStr.lower() == 'z':
+            self._rotationOp = operators.Jz
+        else:
+            raise ValueError('unknown axis')
+
+    def _rotMat(self):
+        if ((self._paramBoundBase__matrix is None) or (self._paramBoundBase__paramUpdated is True)): # pylint: disable=no-member
+            sys = list(self.subSys.values())
+            rotOp = self._rotationOp
+            flipOp = operators.compositeOp(rotOp(sys[0].dimension, isDim=True), sys[0]._dimsBefore, sys[0]._dimsAfter) # pylint: disable=no-member,line-too-long # noqa: E501
+            flipUn = evolution.Unitary(self.phase*self.angle*flipOp)
+            for i in range(len(sys)-1):
+                flipOpN = operators.compositeOp(rotOp(sys[i+1].dimension, isDim=True),
+                                                sys[i+1]._dimsBefore, sys[i+1]._dimsAfter)
+                flipUn = evolution.Unitary(self.phase*self.angle*flipOpN) @ flipUn
+            self._paramBoundBase__matrix = flipOp # pylint: disable=assigning-non-slot
+        self._paramBoundBase__paramUpdated = False # pylint: disable=assigning-non-slot
+        return self._paramBoundBase__matrix # pylint: disable=no-member
+
+class xGate(SpinRotation): # pylint: disable=too-many-ancestors
     label = 'xGate'
     #: (**class attribute**) number of instances created internally by the library
     _internalInstances: int = 0
@@ -12,88 +68,22 @@ class xGate(Gate): # pylint: disable=too-many-ancestors
     _externalInstances: int = 0
     #: (**class attribute**) number of total instances = _internalInstances + _externalInstances
     _instances: int = 0
-    __slots__ = ['__angle', 'phase']
+    __slots__ = []
     def __init__(self, **kwargs):
         super().__init__()
-        self.__angle = None
-        self.phase = 1
+        self.rotationAxis = 'x'
         #self._createUnitary = self._gateImplements
         self._named__setKwargs(**kwargs) # pylint: disable=no-member
-
-    @property
-    def angle(self):
-        return self._xGate__angle
-
-    @angle.setter
-    def angle(self, val):
-        self._xGate__angle = val # pylint: disable=assigning-non-slot
 
     def instantFlip(self):
-        if self._paramBoundBase__matrix is None: # pylint: disable=no-member
-            sys = list(self.subSys.values())
-            # FIXME below is a pi rotation for a spin of any spin quantum number j, thus uses exponential
-            # FIXME BE CAREFUL ABOUT JX VS SIGMAX
-            flipOp = operators.compositeOp(operators.Jx(sys[0].dimension, isDim=True),
-                                           sys[0]._dimsBefore, sys[0]._dimsAfter) # pylint: disable=no-member
-            # if sys[0].dimension == 2:
-            #     flipOp = flipOp
-            # else:
-            flipOp = evolution.Unitary(self.phase*np.pi*flipOp)
-
-            for i in range(len(sys)-1):
-                flipOpN = operators.compositeOp(operators.Jx(sys[i].dimension, isDim=True),
-                                                sys[i+1]._dimsBefore, sys[i+1]._dimsAfter)
-                # if sys[0].dimension == 2:
-                #     flipOpN = flipOpN
-                # else:
-                flipOpN = evolution.Unitary(self.phase*np.pi*flipOpN)
-
-                flipOp = flipOpN @ flipOp
-            self._paramBoundBase__matrix = flipOp # pylint: disable=assigning-non-slot
-        self._paramBoundBase__paramUpdated = False # pylint: disable=assigning-non-slot
-        return self._paramBoundBase__matrix # pylint: disable=no-member
-
-    def _gateImplements(self):
-        if self.implementation.lower() == 'instant':
-            unitary = self.instantFlip()
-            #self.fixed = True
-        return unitary
-
-class rotation(Gate): # pylint: disable=too-many-ancestors
-    label = 'rotation'
-    #: (**class attribute**) number of instances created internally by the library
-    _internalInstances: int = 0
-    #: (**class attribute**) number of instances created explicitly by the user
-    _externalInstances: int = 0
-    #: (**class attribute**) number of total instances = _internalInstances + _externalInstances
-    _instances: int = 0
-    __slots__ = ['__angle', 'rotationAxis']
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.__angle = None
-        self.rotationAxis = None
-        self.implementation = 'instant'
-        #self._createUnitary = self._gateImplements
-        self._named__setKwargs(**kwargs) # pylint: disable=no-member
-
-    @property
-    def angle(self):
-        return self._rotation__angle
-
-    @angle.setter
-    def angle(self, val):
-        self._rotation__angle = val # pylint: disable=assigning-non-slot
-
-    def _rotMat(self):
-        if self._paramBoundBase__matrix is None: # pylint: disable=no-member
+        if ((self._paramBoundBase__matrix is None) or (self._paramBoundBase__paramUpdated is True)): # pylint: disable=no-member
             sys = list(self.subSys.values())
             if self.rotationAxis.lower() == 'x':
-                rotOp = qubitRotations.xRotation
+                rotOp = spinRotations.xRotation
             elif self.rotationAxis.lower() == 'y':
-                rotOp = qubitRotations.yRotation
+                rotOp = spinRotations.yRotation
             elif self.rotationAxis.lower() == 'z':
-                rotOp = qubitRotations.zRotation
-
+                rotOp = spinRotations.zRotation
             flipOp = operators.compositeOp(rotOp(self.angle), sys[0]._dimsBefore, sys[0]._dimsAfter) # pylint: disable=no-member
             for i in range(len(sys)-1):
                 flipOp = operators.compositeOp(rotOp(self.angle), sys[i+1]._dimsBefore, sys[i+1]._dimsAfter) @ flipOp
@@ -102,10 +92,11 @@ class rotation(Gate): # pylint: disable=too-many-ancestors
         return self._paramBoundBase__matrix # pylint: disable=no-member
 
     def _gateImplements(self):
-        if self.implementation.lower() == 'instant':
+        if self.implementation.lower() in ('instant', 'flip'): # pylint: disable=no-member
+            unitary = self.instantFlip()
+        else:
             unitary = self._rotMat()
-            #self.fixed = True
         return unitary
 
-rotation._createUnitary = rotation._gateImplements # pylint: disable=protected-access
+SpinRotation._createUnitary = SpinRotation._rotMat # pylint: disable=protected-access
 xGate._createUnitary = xGate._gateImplements