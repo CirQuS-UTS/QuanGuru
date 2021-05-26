55cd37d1cb622a5f6abc5699ab60fe6c00d1e6af
cahitkargi <ckargi88@gmail.com>
2021-05-27 02:09:41 +1000

    docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -272,8 +272,8 @@ sphinx_gallery_line_numbers = True
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-templates_path = ['_templates']
+html_static_path = ['static']
+templates_path = ['templates']
 html_css_files = ['style.css', 'custom.css', 'gallery.css']
 
 # html_logo = 'images/logo.png'
