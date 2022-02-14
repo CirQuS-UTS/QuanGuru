de6c20890025be8596d3cb6fe4a1cc04fef8660f
cahitkargi <ckargi88@gmail.com>
2022-02-14 11:41:08 +1100

    initial state creation tests parametrized and updated

--- tests/classes/QuantumSystem/test_initialStateCreation.py
+++ tests/classes/QuantumSystem/test_initialStateCreation.py
@@ -1,39 +1,58 @@
 import numpy as np
+import pytest
 import quanguru.classes.QSystem as QSys
 import quanguru.QuantumToolbox.states as qSts
+from quanguru.classes.QSys import QuantumSystem
 
-def test_createInitialStateForSingleSystem():
-    cls = QSys.QuSystem
+@pytest.mark.parametrize("cls", [
+                         QuantumSystem,
+                         QSys.QuSystem
+                         ])
+def test_createInitialStateForSingleSystem(cls):
     qsys = cls(dimension=4)
 
-    initState = qsys._createInitialState(2)
+    initState = qsys._createAstate(2)
     assert initState[0] == 0
     assert initState[1] == 0
     assert initState[2] == 1
     assert initState[3] == 0
 
-    initState = qsys._createInitialState([1, 3])
+    initState = qsys._createAstate([1, 3])
     assert initState[0] == 0
     assert initState[1] == 1/np.sqrt(2)
     assert initState[2] == 0
     assert initState[3] == 1/np.sqrt(2)
 
-    initState = qsys._createInitialState([0, 1, 3])
+    initState = qsys._createAstate([0, 1, 3])
     assert initState[0] == 1/np.sqrt(3)
     assert initState[1] == 1/np.sqrt(3)
     assert initState[2] == 0
     assert initState[3] == 1/np.sqrt(3)
 
-    initState = qsys._createInitialState({0:0.1, 1:0.2, 2:0.3, 3:0.4})
+    initState = qsys._createAstate({0:0.1, 1:0.2, 2:0.3, 3:0.4})
     assert np.round(initState[0], 14) == np.round(np.sqrt(0.1), 14)
     assert np.round(initState[1], 14) == np.round(np.sqrt(0.2), 14)
     assert np.round(initState[2], 14) == np.round(np.sqrt(0.3), 14)
     assert np.round(initState[3], 14) == np.round(np.sqrt(0.4), 14)
 
     qsys._inpCoef = True
-    initState = qsys._createInitialState({0:0.2*(1+1j), 2:0.2})
+    initState = qsys._createAstate({0:0.2*(1+1j), 2:0.2})
     assert np.round(initState[0].real, 8) == 0.57735027
     assert np.round(initState[0].imag, 8) == 0.57735027
     assert initState[1] == 0
     assert np.round(initState[2], 8) == 0.57735027
     assert initState[3] == 0
+
+    with pytest.raises(ValueError):
+        initState = qsys._createAstate(qSts.densityMatrix([qSts.basis(2, 1), qSts.basis(2, 0)], [0.5, 0.5]))
+
+    initState = qsys._createAstate(qSts.densityMatrix([qSts.basis(4, 1), qSts.basis(4, 0)], [0.25, 0.75]))
+    print(initState.A)
+    for ind1 in range(4):
+        for ind2 in range(4):
+            if ((ind1 == 0) and (ind2 == 0)):
+                initState[ind1][:, ind2] = 0.25
+            elif ((ind1 == 1) and (ind2 == 1)):
+                initState[ind1][:, ind2] = 0.75
+            else:
+                initState[ind1][:, ind2] = 0
