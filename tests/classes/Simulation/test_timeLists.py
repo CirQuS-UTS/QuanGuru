f10e72b45fc124f655cd91c98f8382cb5f00dbde
Adrien Di Lonardo <13277342@student.uts.edu.au>
2022-11-09 15:27:02 +1100

    Changed test values + added documentation

--- tests/classes/Simulation/test_timeLists.py
+++ tests/classes/Simulation/test_timeLists.py
@@ -4,6 +4,13 @@ from itertools import permutations
 import quanguru as qg
 
 def assertHelper(lastAssigned, vals, timeList):
+    """
+    Helper function to test the timelists have the correct:
+        - length
+        - stepSize
+        - final time step is smaller than the total time
+        - final time step is within one step of the total time
+    """
     if lastAssigned == 'totalTime': vals['totalTime'] = vals["stepSize"]*vals["stepCount"]
     if lastAssigned == 'stepSize': vals['stepSize'] = vals["totalTime"]/vals["stepCount"]
     if lastAssigned == 'stepCount': vals['stepCount'] = int(vals["totalTime"]/vals["stepSize"])
@@ -13,19 +20,28 @@ def assertHelper(lastAssigned, vals, timeList):
     assert 0 <= vals['totalTime'] - timeList[-1]
     assert abs(vals['totalTime'] - timeList[-1]) < vals['stepSize']
 
+def generateTestVals():
+    """
+    Helper function to generate random test values for the time parameters
+    """
+    valsForTest = {
+        "totalTime": round(random.uniform(5, 50), 2),
+        "stepCount": random.randint(5, 20),
+        "stepSize": round(random.uniform(1, 4), 2),
+    }
+    return valsForTest
+
 def test_calculateTimeListTwoDefined():
-    # Testing that the third time parameter out of the three is correctly calculated and assigned when only the other two are defined
+    """
+    Testing that the time list is correctly defined when only two are manually assigned
+    """
     timeParameters = ['totalTime', 'stepSize', 'stepCount']
 
     # Assign time parameters in the order of every permutation of two of the time parameters above
     for order in permutations(timeParameters):
         sim = qg.Simulation()
 
-        valsForTest = {
-            "totalTime": random.uniform(5, 50),
-            "stepCount": random.randint(5, 20),
-            "stepSize": random.uniform(1, 4),
-        }
+        valsForTest = generateTestVals()
 
         setattr(sim, order[0], valsForTest[order[0]])
         setattr(sim, order[1], valsForTest[order[1]])
@@ -35,18 +51,16 @@ def test_calculateTimeListTwoDefined():
         del sim
 
 def test_calculateTimeListThreeDefined():
-    # Testing that the third time parameter out of the three is correctly calculated and assigned when only the other two are defined
+    """
+    Testing that the time list is correctly defined when all three are manually assigned
+    """
     timeParameters = ['totalTime', 'stepSize', 'stepCount']
 
-    # Assign time parameters in the order of every permutation of two of the time parameters above
+    # Assign time parameters in the order of every permutation of the time parameters above
     for order in permutations(timeParameters):
         sim = qg.Simulation()
 
-        valsForTest = {
-            "totalTime": random.uniform(5, 50),
-            "stepCount": random.randint(5, 20),
-            "stepSize": random.uniform(1, 4),
-        }
+        valsForTest = generateTestVals()
 
         setattr(sim, order[2], valsForTest[order[2]])
         setattr(sim, order[0], valsForTest[order[0]])
@@ -57,18 +71,16 @@ def test_calculateTimeListThreeDefined():
         del sim
 
 def test_calculateTimeListSixDefined():
-    # Testing that the third time parameter out of the three is correctly calculated and assigned when only the other two are defined
+    """
+    Testing all three time parameters are correctly defined when all three are manually assigned and one is assigned repeatedly
+    """
     timeParameters = ['totalTime', 'stepSize', 'stepCount']
 
-    # Assign time parameters in the order of every permutation of two of the time parameters above
+    # Assign time parameters in the order of every permutation of the time parameters above
     for order in permutations(timeParameters):
         sim = qg.Simulation()
 
-        valsForTest = {
-            "totalTime": random.uniform(5, 50),
-            "stepCount": random.randint(5, 20),
-            "stepSize": random.uniform(1, 4),
-        }
+        valsForTest = generateTestVals()
 
         setattr(sim, order[2], valsForTest[order[2]])
         setattr(sim, order[0], valsForTest[order[0]])
--- tests/classes/baseOthers/test_QSimBase.py
+++ tests/classes/baseOthers/test_QSimBase.py
@@ -4,6 +4,9 @@ from itertools import permutations
 import quanguru as qg
 
 def assertHelper(lastAssigned, vals, timeBase):
+    """
+    Helper function to test the assertions for each test of the time parameters
+    """
     if lastAssigned == 'totalTime': vals['totalTime'] = vals["stepSize"]*vals["stepCount"]
     if lastAssigned == 'stepSize': vals['stepSize'] = vals["totalTime"]/vals["stepCount"]
     if lastAssigned == 'stepCount': vals['stepCount'] = int(vals["totalTime"]/vals["stepSize"])
@@ -12,19 +15,29 @@ def assertHelper(lastAssigned, vals, timeBase):
     assert timeBase._timeBase__stepSize.value == vals["stepSize"]
     assert timeBase._timeBase__stepCount.value == vals["stepCount"]
 
-def test_calculateThirdTimeParameterUnassigned():
-    # Testing that the third time parameter out of the three is correctly calculated and assigned when only the other two are defined
+def generateTestVals():
+    """
+    Helper function to generate random test values for the time parameters
+    """
+    valsForTest = {
+        "totalTime": round(random.uniform(5, 50), 2),
+        "stepCount": random.randint(5, 20),
+        "stepSize": round(random.uniform(1, 4), 2),
+    }
+    return valsForTest
+
+def test_calculateTimeParameterTwoDefined():
+    """
+    Testing all three time parameters are correctly defined when only two are manually assigned
+    (The unassigned parameter should be calculated based on the values of the other two)
+    """
     timeParameters = ['totalTime', 'stepSize', 'stepCount']
 
     # Assign time parameters in the order of every permutation of two of the time parameters above
     for order in permutations(timeParameters):
         tim = qg.QSimBase.timeBase()
 
-        valsForTest = {
-            "totalTime": random.uniform(5, 50),
-            "stepCount": random.randint(5, 20),
-            "stepSize": random.uniform(1, 4),
-        }
+        valsForTest = generateTestVals()
 
         setattr(tim, order[0], valsForTest[order[0]])
         setattr(tim, order[1], valsForTest[order[1]])
@@ -33,19 +46,18 @@ def test_calculateThirdTimeParameterUnassigned():
 
         del tim
 
-def test_calculateThirdTimeParameterAssigned():
-    # Testing that the third time parameter out of the three is correctly calculated and assigned when only the other two are defined
+def test_calculateTimeParameterThreeDefined():
+    """
+    Testing all three time parameters are correctly defined when all three are manually assigned
+    (The least recent defined parameter should be overwritten based on the two most recently defined)
+    """
     timeParameters = ['totalTime', 'stepSize', 'stepCount']
 
-    # Assign time parameters in the order of every permutation of two of the time parameters above
+    # Assign time parameters in the order of every permutation of the time parameters above
     for order in permutations(timeParameters):
         tim = qg.QSimBase.timeBase()
 
-        valsForTest = {
-            "totalTime": random.uniform(5, 50),
-            "stepCount": random.randint(5, 20),
-            "stepSize": random.uniform(1, 4),
-        }
+        valsForTest = generateTestVals()
 
         #Assigning the third time parameter to see if it gets overwritten
         setattr(tim, order[2], valsForTest[order[2]])
@@ -56,21 +68,20 @@ def test_calculateThirdTimeParameterAssigned():
 
         del tim
 
-def test_calculateThirdTimeParameterRepeatedAssignment():
-    # Testing that the third time parameter out of the three is correctly calculated and assigned when only the other two are defined
+def test_calculateTimeParameterSixDefined():
+    """
+    Testing all three time parameters are correctly defined when all three are manually assigned and one is assigned repeatedly
+    (The least recent defined parameter should be overwritten based on the two most recently defined)
+    (Only the most recent value assignment from the repeated assignment should be used)
+    """
     timeParameters = ['totalTime', 'stepSize', 'stepCount']
 
-    # Assign time parameters in the order of every permutation of two of the time parameters above
+    # Assign time parameters in the order of every permutation of the time parameters above
     for order in permutations(timeParameters):
         tim = qg.QSimBase.timeBase()
 
-        valsForTest = {
-            "totalTime": random.uniform(5, 50),
-            "stepCount": random.randint(5, 20),
-            "stepSize": random.uniform(1, 4),
-        }
+        valsForTest = generateTestVals()
 
-        #Assigning the third time parameter to see if it gets overwritten
         setattr(tim, order[2], valsForTest[order[2]])
         setattr(tim, order[0], valsForTest[order[0]])
         setattr(tim, order[1], valsForTest[order[1]])
@@ -82,11 +93,7 @@ def test_calculateThirdTimeParameterRepeatedAssignment():
             if order[1] == 'stepSize': valsForTest[order[1]] = random.uniform(1, 4)
 
             setattr(tim, order[1], valsForTest[order[1]])
-
-        assertHelper(order[2], valsForTest, tim)
+            
+            assertHelper(order[2], valsForTest, tim)
 
         del tim
-
-#Tests for;
-# - correct timeLists from params (length, stepCount, 0 <= timeList[-1] - totalTime < stepSize)
-# - timeLists being correctly calculated from JUST the last two defined timeParameters
