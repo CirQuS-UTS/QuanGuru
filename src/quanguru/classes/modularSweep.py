de5a1a9f93f36c1e716b22b1dd33a30109edba58
Joshua Tran <infernixslug@gmail.com>
2025-08-21 14:31:53 +1000

    Simpler fix implemented

--- src/quanguru/classes/modularSweep.py
+++ src/quanguru/classes/modularSweep.py
@@ -244,10 +244,10 @@ def timeDependent(qSim):
         #  if there are multiple sweeps when using sweeps for time-dependency, all of them has to run simultaneously.
         #  if they are combinatorial multi-parameter sweep, the behavior is undefined.
         #  labels: undefined behaviour
-        qSim._timeBase__stepCount._value = qSim.timeDependency.indMultip # pylint: disable=protected-access
-        for pro, sim in qSim.subSys.items(): # pylint: disable=protected-access
-            sim.simulation._timeBase__stepCount._value = sim.simulation.timeDependency.indMultip_intervals # pylint: disable=protected-access
-            pro.simulation._timeBase__stepCount._value = pro.simulation.timeDependency.indMultip_intervals # pylint: disable=protected-access
+        # qSim._timeBase__stepCount._value = qSim.timeDependency.indMultip # pylint: disable=protected-access
+        # for pro, sim in qSim.subSys.items(): # pylint: disable=protected-access
+        #     sim.simulation._timeBase__stepCount._value = sim.simulation.timeDependency.indMultip_intervals # pylint: disable=protected-access
+        #     pro.simulation._timeBase__stepCount._value = pro.simulation.timeDependency.indMultip_intervals # pylint: disable=protected-access
     timeEvolDefault(qSim, td)
 
 
--- tests/classes/Simulation/test_timeLists.py
+++ tests/classes/Simulation/test_timeLists.py
@@ -151,5 +151,3 @@ def test_stepCountAndTimeListOnSweepWithTimeDependecy():
 
 
 
-
-
