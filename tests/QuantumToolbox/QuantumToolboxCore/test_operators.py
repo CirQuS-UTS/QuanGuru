d177704282a4f0dc0944c17865726d6fdb9f5c2d
Treerat Srivipat <treerat.srivipat@student.uts.edu.au>
2025-09-02 16:40:27 +1000

    add tests for random H operators

--- tests/QuantumToolbox/QuantumToolboxCore/test_operators.py
+++ tests/QuantumToolbox/QuantumToolboxCore/test_operators.py
@@ -2,9 +2,9 @@ import random as rn
 import numpy as np
 import pytest
 from quanguru.QuantumToolbox import linearAlgebra as la#pylint: disable=import-error
-from quanguru.QuantumToolbox import states#pylint: disable=import-error
+from quanguru.QuantumToolbox import states, IPR#pylint: disable=import-error
 from quanguru.QuantumToolbox import operators as ops #pylint: disable=import-error
-
+from scipy import linalg
 
 def checkGivenRuleForAnArray(cOp, rule, *args):
     # assert correctness of elements in a matrix by comparing against a given rule that calculated the expected value
@@ -89,10 +89,55 @@ def test_displacement():
     # coherent state is the eigenstate of destroy with eigenvalue alpha
     assert np.allclose((ops.destroy(20)@displacedVacuum).A, (alpha*displacedVacuum).A, atol=1e-04, rtol=1e-04)
 
-def test_goeH():
-
+def test_randomH():
+    # random Hamiltonian with no symmetry and unnormalised
+    op = ops.randomH(dimension=1000, normalise=False, symmetric=False)
     # check hermitian
-    assert np.allclose(ops.goeH.T.conj(), ops.goeH)
-    # # check matrix entries are real number only
-    # ops.goeH(dimension=1000)
+    assert np.allclose(op.T.conj(), op)
+    # check no symmetry
+    assert not np.allclose(op, op.T)
 
+    # random Hamiltonian with symmetry and unnormalised
+    op = ops.randomH(dimension=1000, normalise=False, symmetric=True)
+    # check hermitian
+    assert np.allclose(op.T.conj(), op)
+    # check symmetry
+    assert np.allclose(op, op.T)
+
+    # random Hamiltonian with symmetry and normalised
+    op = ops.randomH(dimension=1000, normalise=True, symmetric=True)
+    # check hermitian
+    assert np.allclose(op.T.conj(), op)
+    # check symmetry
+    assert np.allclose(op, op.T)
+    # check normalisation
+    assert np.round(linalg.norm(op), 7) == 1
+
+def test_goeH():
+    op = ops.goeH(dimension=1000)
+    # check hermitian
+    assert np.allclose(op.T.conj(), op)
+    # check matrix entries are real number only
+    assert np.allclose(op.imag, np.zeros(op.shape))
+    # check IPR of eigenvectors
+    val, vec = linalg.eig(op)
+    assert np.allclose(round(IPR.iprKetNB(vec), 2), 0.33, atol=0.01)
+
+def test_gueH():
+    op = ops.gueH(dimension=1000)
+    # check hermitian
+    assert np.allclose(op.T.conj(), op)
+    # check no symmetry
+    assert not np.allclose(op, op.T)
+    # check IPR of eigenvectors
+    val, vec = linalg.eig(op)
+    assert np.allclose(round(IPR.iprKetNB(vec), 2), 0.50, atol=0.01)
+
+def test_gseH():
+    op = ops.gseH(dimension=1000)
+    # check dimension of matrix (2*dim, 2*dim)
+    assert op.shape == (2000, 2000)
+    # check hermitian
+    assert np.allclose(op.T.conj(), op)
+    # check no symmetry
+    assert not np.allclose(op, op.T)
